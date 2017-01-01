0b01ecadb92d5f33636855157f8460bff04cbff4
Denis Korobicyn <deniskorobitcin@gmail.com>
2017-01-01 15:51:33 +0500

    added visual studio 17 CmakeSetting.json

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <yaml-cpp/yaml.h>
 
-
 int main() {
   std::cout << "Hello, world!" << std::endl;
-  YAML::Node node = YAML::Load("{name: Brewers, city: Milwaukee}");
-  std::cout << node.size();
+  try {
+	  YAML::Node config = YAML::LoadFile("dip.yml");
+	  std::cout << config.size() << std::endl;
+  }
+  catch (YAML::BadFile e) {
+	  std::cout << "File dip.yml is not found" << std::endl;
+  }
+
   return 0;
 }
