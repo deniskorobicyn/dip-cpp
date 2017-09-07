9b421063d3e5416b51f21d97a400a1d7913f70ae
Denis Korobicyn <deniskorobitcin@gmail.com>
2017-09-08 00:38:47 +0500

    Fix dnsdock

--- src/commands/dnsdock.cpp
+++ src/commands/dnsdock.cpp
@@ -8,11 +8,11 @@ dip::Dnsdock::Dnsdock(dip::Dip *dip)
 
 void dip::Dnsdock::run(std::string args)
 {
-	if (0 == args.compare(0, 2, "up")) {
+	if (0 == args.compare(0, 4, "\"up\"")) {
 		system("docker run --detach --volume /var/run/docker.sock:/var/run/docker.sock:ro --restart always --publish 53:53/udp --name=dnsdock aacebedo/dnsdock:latest-amd64");
 	}
 
-	if (0 == args.compare(0, 4, "down")) {
+	if (0 == args.compare(0, 6, "\"down\"")) {
 		system("docker stop dnsdock");
 		system("docker rm -v dnsdock");
 	}
